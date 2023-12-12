09e4abb56b6b4b029467c14599c949d97cce948c
David Jed <Dev.Davidjed@gmail.com>
2023-12-11 23:33:45 -0500

    Fixed Routing bug

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './elements/navigation';
 import About from './pages/about';
 import Contact from './pages/contact';
