4ebc07133ce5e5ec18895a86d3bbd03c0fa794f4
MindlessTruffle <82486398+MindlessTruffle@users.noreply.github.com>
2023-12-12 00:13:21 -0500

    Update about.tsx

--- vite-project/src/pages/about.tsx
+++ vite-project/src/pages/about.tsx
@@ -6,7 +6,7 @@ function About() {
         <div className="about-content">
           <h1>About Us</h1>
           <p>We are a passionate team dedicated to delivering quality products to our customers. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <img src="team-image.jpg" alt="No Team Image Found" />
+          <img src={TeamImage} alt="No Team Image Found" />
         </div>
       </section>
     </main>
